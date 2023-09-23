ed554f123dfed4b8bfada3e15f1f205851b10c71
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-09-23 14:45:43 -0500

    Revert "Delete appComponent"
    This reverts commit 60dff7d94e6b986789d711598ce28ef5d280ccde.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
